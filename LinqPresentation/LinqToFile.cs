12a7caab58e547fe5675d812b31e808b741f7d00
FrelV <FrelV@ACER>
2017-11-11 18:14:42 +0100

    Working version

---
+++ LinqPresentation/LinqToFile.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LinqPresentation
+{
+    public partial class MainWindow
+    {
+        #region struct
+
+        public struct Movie
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public int Year { get; set; }
+            public int Lenght { get; set; }
+
+            public Movie(int iD, string name, int year, int lenght)
+            {
+                ID = iD;
+                Name = name ?? throw new ArgumentNullException(nameof(name));
+                Year = year;
+                Lenght = lenght;
+            }
+        }
+
+        public struct People
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public string Surname { get; set; }
+
+            public People(int iD, string name, string surname)
+            {
+                ID = iD;
+                Name = name ?? throw new ArgumentNullException(nameof(name));
+                Surname = surname ?? throw new ArgumentNullException(nameof(surname));
+            }
+        }
+
+        public struct Director
+        {
+            public int FilmId { get; set; }
+            public int PeopleId { get; set; }
+
+            public Director(int filmId, int peopleId)
+            {
+                FilmId = filmId;
+                PeopleId = peopleId;
+            }
+        }
+
+        public struct Role
+        {
+            public int filmId { get; set; }
+            public int peopleId { get; set; }
+            public string roleName { get; set; }
+
+            public Role(int filmId, int peopleId, string roleName)
+            {
+                this.filmId = filmId;
+                this.peopleId = peopleId;
+                this.roleName = roleName ?? throw new ArgumentNullException(nameof(roleName));
+            }
+        }
+
+        public struct Query
+        {
+            public string Name { get; set; }
+        }
+
+        #endregion
+
+        public IEnumerable<Movie> Movies;
+        public IEnumerable<Director> Directors;
+        public IEnumerable<Role> Roles;
+        public string[] files = {"Movies", "Directors", "Peoples", "Roles"};
+
+        private void DataShow(DataGrid targetDataGrid, ComboBox sourceCheckBox)
+        {
+            switch (sourceCheckBox.SelectedValue)
+            {
+                case "Movies":
+                    targetDataGrid.ItemsSource = Movies;
+                    break;
+                case "Directors":
+                    targetDataGrid.ItemsSource = Directors;
+                    break;
+                case "Peoples":
+                    targetDataGrid.ItemsSource = Peoples;
+                    break;
+                case "Roles":
+                    targetDataGrid.ItemsSource = Roles;
+                    break;
+                default:
+                    targetDataGrid.ItemsSource = null;
+                    break;
+            }
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataShow(DataShowMemory, DataToShow);
+        }
+
+        private void LoadToMemory_Click(object sender, RoutedEventArgs e)
+        {
+            Movies = Properties.Resources.movie.Split('\n')
+                .Where(csvLine => !string.IsNullOrWhiteSpace(csvLine))
+                .Select(csvLine => new Movie(Convert.ToInt32(csvLine.Split(';')[0]), csvLine.Split(';')[1],
+                    Convert.ToInt32(csvLine.Split(';')[2]), Convert.ToInt32(csvLine.Split(';')[3])));
+            Directors = Properties.Resources.director.Split('\n')
+                .Where(csvLine => !string.IsNullOrWhiteSpace(csvLine))
+                .Select(csvLine =>
+                    new Director(Convert.ToInt32(csvLine.Split(';')[0]), Convert.ToInt32(csvLine.Split(';')[1])));
+
+            Peoples = Properties.Resources.people.Split('\n')
+                .Where(csvLine => !string.IsNullOrWhiteSpace(csvLine))
+                .Select(csvLine => new People(Convert.ToInt32(csvLine.Split(';')[0]), csvLine.Split(';')[1],
+                    csvLine.Split(';')[2]));
+            Roles = Properties.Resources.role.Split('\n')
+                .Where(csvLine => !string.IsNullOrWhiteSpace(csvLine))
+                .Select(csvLine => new Role(Convert.ToInt32(csvLine.Split(';')[0]),
+                    Convert.ToInt32(csvLine.Split(';')[1]), csvLine.Split(';')[2]));
+        }
+    }
+}
---
+++ LinqPresentation/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xml.Linq;
+
+namespace LinqPresentation
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        public string[] Queries =
+        {
+            "Films longer than 2h",
+            "Count films by director",
+            "1990's characters",
+            "All films",
+            "All Peoples",
+            "All Directors"
+        };
+        public IEnumerable<People> Peoples;
+
+        public LinqDB DataBase;
+
+        //public IEnumerable<string[]> DataSource = 
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataToShow.ItemsSource = files;
+            DataToShowXML.ItemsSource = files;
+            QueryToShow.ItemsSource = Queries;
+        }
+
+        private void DataToShowXML_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void LoadFromXML_Click(object sender, RoutedEventArgs e)
+        {
+
+            var Directors = XDocument
+                .Load(@"C:\\Users\\FrelV\\source\\repos\\LinqPresentation\\LinqPresentation\\Data\\Directors.xml").Elements().Select(node => new Director((int)node.Element("FilmId"),(int)node.Element("PeopleId")));
+            XMLToShow.ItemsSource = Directors;
+        }
+
+        private void ConnectDB_Click(object sender, RoutedEventArgs e)
+        {
+            DataBase = new LinqDB("Data Source=linqsql.database.windows.net;Initial Catalog=LinqDB;Integrated Security=False;User ID=FrelVick;Password=Pogosty$1010;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            var longFilms = DataBase.Movies.Where(movie => movie.Length >= 120);
+            var Directors = DataBase.Directors.GroupBy(direct => direct.People)
+                .Select(grouping => new
+                {
+                    grouping.Key.Name,
+                    grouping.Key.Surname,
+                    FilmCount = grouping.Count()
+                });
+
+
+            DataShowDB.ItemsSource = Directors;
+
+
+        }
+
+        private void QueryToShow_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (QueryToShow.SelectedItem)
+            {
+                case "Films longer than 2h":
+                    DataShowDB.ItemsSource = DataBase.Movies.Where(movie => movie.Length >= 120);
+                    break;
+                case "Count films by director":
+                    DataShowDB.ItemsSource = DataBase.Directors.GroupBy(direct => direct.People)
+                        .Select(grouping => new
+                        {
+                            grouping.Key.Name,
+                            grouping.Key.Surname,
+                            FilmCount = grouping.Count()
+                        }).OrderBy(d => d.FilmCount);
+                    break;
+                case "All films":
+                    DataShowDB.ItemsSource = DataBase.Movies;
+                    break;
+                case "1990's characters":
+                    DataShowDB.ItemsSource = DataBase.Roles
+                        .Join(DataBase.Movies, r => r.FilmId, m => m.Id, (r, m) => new {r, m})
+                        .Where(t => t.m.Year == 1990)
+                        .Select(t => new
+                        {
+                            t.m.Year,
+                            Movie = t.m.Name,
+                            Role = t.r.Role1
+                        });
+                    /* same querie
+                    from r in DataBase.Roles
+                    join m in DataBase.Movies on r.FilmId equals m.Id
+                    where m.Year == 1990
+                    select new
+                    {
+                        Year = m.Year,
+                        Movie = m.Name,
+                        Role = r.Role1
+                    };
+                    */
+                    break;
+                case "All Peoples":
+                    DataShowDB.ItemsSource = DataBase.Peoples; break;
+                case "All Directors":
+                    DataShowDB.ItemsSource = DataBase.Directors.GroupBy(d => d.People).Select(d => new
+                    {
+                        d.Key.Name,
+                        d.Key.Surname,
+                        Film = string.Join(", ", d.Select(i => i.Movie.Name))
+                    }); break;
+            }
+        }
+    }
+}
